9258dc5c1183a7503eda38c20603b9758cf63f77
Jacob Bryan <jtb36688@gmail.com>
2019-02-22 12:14:28 -0500

    Finished Handlers
    Finished handlers which help return actions as an array of objects within a property of the specified GET of a project.

--- data/rdbhandlers.js
+++ data/rdbhandlers.js
@@ -5,7 +5,10 @@ const db = knex(knexConfig.development);
 module.exports = {
   addProject,
   getProjects,
-  getProjectActions,
+  removeProject,
+  modifyProject,
+  addAction,
+  getAction
 };
 
 function addProject(request) {
@@ -19,29 +22,65 @@ function getProjects(id) {
     query.where("id", id).first();
     return Promise.all([query, this.getProjectActions(id)]).then(results => {
       let [Project, Actions] = results;
+      if (Project) {
         Project.Actions = Actions;
         return ProjectToBody(Project);
-      })}
-    return query;
-  }
-
-
-
-function getAction(id) {
-  let query = db("Actions");
-
-  if (id && id > 0) {
-    query.where("id", id).first();
-    return Promise.all([query, getActionIngredients(id)]).then(results => {
-      let [Action, ingredients] = results;
-      if (Action) {
-        Action.ingredients = ingredients;
-        return ActionToBody(Action);
       } else {
         return null;
       }
-    })
+    });
   }
-  return query
+  return query;
 }
 
+function getProjectActions(id) {
+  return db("actions")
+    .where("project_id", id)
+    .then(Actions => Actions.map(Action => ActionToBody(Action)));
+}
+
+function removeProject(id) {
+  return db("projects")
+    .where({ id: id })
+    .del();
+}
+
+async function modifyProject(id, request) {
+  const conditional = await db('projects')
+    .where("id", Number(id))
+    .update(request);
+  if (conditional) {
+    return getById(id);
+  } else {
+    return null;
+  }
+}
+
+function addAction(request) {
+    return db("actions").insert(request)
+}
+
+function ProjectToBody(project) {
+  const result = {
+    ...project,
+    completed: intToBoolean(project.completed),
+  };
+  if (project.actions) {
+    result.Actions = project.actions.map(action => ({
+      ...action,
+      completed: intToBoolean(action.completed),
+    }));
+  }
+  return result;
+}
+
+function ActionToBody(action) {
+    return {
+      ...action,
+      completed: intToBoolean(action.completed),
+    };
+  }
+
+  function intToBoolean(int) {
+    return int === 1 ? true : false
+  }
