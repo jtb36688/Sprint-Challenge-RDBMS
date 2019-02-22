47ea858ed445f92e9da8949e9a9bb67e51a3187d
Jacob Bryan <jtb36688@gmail.com>
2019-02-22 11:47:50 -0500

    Added foreign key to actions in migrations referring to projects

--- migrations/20190222113522_project.js
+++ migrations/20190222113522_project.js
@@ -8,6 +8,13 @@ exports.up = function(knex, Promise) {
     })
     .createTable("actions", column => {
       column.increments();
+      column
+        .integer("project_id")
+        .unsigned()
+        .references("id")
+        .inTable("projects")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
       column.string("description", 256).notNullable();
       column.string("notes", 256);
       column.boolean("completed");
@@ -15,7 +22,5 @@ exports.up = function(knex, Promise) {
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema
-  .dropTableIfExists("projects")
-  .dropTableIfExists("actions");
+  return knex.schema.dropTableIfExists("projects").dropTableIfExists("actions");
 };
