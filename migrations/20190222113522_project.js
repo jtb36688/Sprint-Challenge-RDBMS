8f0e372df7f3b48397fba6e3a5f7a294ce4ff8fc
Jacob Bryan <jtb36688@gmail.com>
2019-02-22 11:46:19 -0500

    laid out schema in migrations
    boilerplated 2 tables with 4 columns each for projects and actions

---
+++ migrations/20190222113522_project.js
@@ -0,0 +1,21 @@
+exports.up = function(knex, Promise) {
+  return knex.schema
+    .createTable("projects", column => {
+      column.increments();
+      column.string("name", 100).notNullable();
+      column.string("description", 256);
+      column.boolean("completed");
+    })
+    .createTable("actions", column => {
+      column.increments();
+      column.string("description", 256).notNullable();
+      column.string("notes", 256);
+      column.boolean("completed");
+    });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema
+  .dropTableIfExists("projects")
+  .dropTableIfExists("actions");
+};
