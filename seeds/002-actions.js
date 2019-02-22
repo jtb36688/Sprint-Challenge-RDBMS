feeec93ceb4893e7524e705edf6db790ca9bd807
Jacob Bryan <jtb36688@gmail.com>
2019-02-22 12:26:35 -0500

    finished seeds
    Created 3 seeds for projects and then several seeds for actions which take project_id as a foreign key and refer to the 3 projects.

--- seeds/002-actions.js
+++ seeds/002-actions.js
@@ -1,13 +1,52 @@
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('actions').del()
-    .then(function () {
+  return knex("actions")
+    .truncate()
+    .then(function() {
       // Inserts seed entries
-      return knex('actions').insert([
-        {name: 'rowValue1'},
-        {name: 'rowValue2'},
-        {name: 'rowValue3'}
+      return knex("actions").insert([
+        {
+          name: "rowValue1",
+          project_id: 1,
+          description: "Great Idea",
+          notes: "beginner",
+          completed: true
+        },
+        {
+          name: "rowValue2",
+          project_id: 1,
+          description: "Party Bus",
+          notes: "intermediate",
+          completed: true
+        },
+        {
+          name: "rowValue3",
+          project_id: 1,
+          description: "Architecture",
+          notes: "Advanced",
+          completed: true
+        },
+        {
+          name: "rowValue1",
+          project_id: 2,
+          description: "Instagram",
+          notes: "Advanced",
+          completed: true
+        },
+        {
+          name: "rowValue2",
+          project_id: 3,
+          description: "Projects",
+          notes: "beginner",
+          completed: true
+        },
+        {
+          name: "rowValue3",
+          project_id: 3,
+          description: "Testing",
+          notes: "intermediate",
+          completed: false
+        }
       ]);
     });
 };
